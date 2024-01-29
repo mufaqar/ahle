d3724b64f8d60a33e8e72bacbbb5ac60bef438a4
mufaqar <mufaqar@gmail.com>
2024-01-29 17:38:45 +0500

    Images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:['ant.a59.mywebsitetransfer.com']
+  },
 };
 
 export default nextConfig;
