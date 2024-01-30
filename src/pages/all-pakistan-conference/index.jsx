3673a59693da250d7ed9a53d0d8aa1371f0c6368
mufaqar <mufaqar@gmail.com>
2024-01-30 20:55:47 +0500

    all pakistan

--- src/pages/all-pakistan-conference/index.jsx
+++ src/pages/all-pakistan-conference/index.jsx
@@ -1,18 +1,17 @@
-"use client"
 import React from 'react';
-import YouTube, { YouTubeProps } from 'react-youtube';
-import { YtVideo } from '../components/YtVideo'
+//import YouTube, { YouTubeProps } from 'react-youtube';
+//import { YtVideo } from '../components/YtVideo';
 import PageBanner from '../../components/banner';
 
-const Page = () => {
-  const onPlayerReady = (event) => {
-    event.target.pauseVideo();
-  }
-  const opts = {
-    playerVars: {
-      autoplay: 0,
-    },
-  };
+export default function AllPakistanConferences() {
+  // const onPlayerReady = (event) => {
+  //   event.target.pauseVideo();
+  // };
+  // const opts = {
+  //   playerVars: {
+  //     autoplay: 0,
+  //   },
+  // };
 
   return (
     <main>
@@ -23,17 +22,13 @@ const Page = () => {
         buttontext=""
         buttonLink=""
       />
-      <section className='container px-4 md:px-10 mx-auto'>
-        <div className='items-center font-ahle my-10 md:my-20 md:mt-20 grid gap-10'>
+      <section className="container px-4 md:px-10 mx-auto">
+        <div className="items-center font-ahle my-10 md:my-20 md:mt-20 grid gap-10">
           <div className="my-10 grid md:grid-cols-3 grid-cols-1 gap-7">
-            <YtVideo opts={opts} onPlayerReady={onPlayerReady} />
+            {/* <YtVideo opts={opts} onPlayerReady={onPlayerReady} /> */}
           </div>
         </div>
       </section>
     </main>
   );
-};
-
-export default Page;
-
-
+}
