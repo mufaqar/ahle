3f7d8f4e941c47c7a7a425eabd54538814ead04e
mufaqar <mufaqar@gmail.com>
2024-01-31 12:04:13 +0500

    dastoor

--- src/const/dastor.jsx
+++ src/const/dastor.jsx
@@ -1,92 +1,92 @@
 export const Dastoor_Data = [
     {
-        img: "/assets/images/dastoor/01.jpg",
+        img: "/images/dastoor/01.jpg",
     },
     {
-        img: "/assets/images/dastoor/1.jpg",
+        img: "/images/dastoor/1.jpg",
     },
     {
-        img: "/assets/images/dastoor/2.jpg",
+        img: "/images/dastoor/2.jpg",
     },
     {
-        img: "/assets/images/dastoor/3.jpg",
+        img: "/images/dastoor/3.jpg",
     },
     {
-        img: "/assets/images/dastoor/4.jpg",
+        img: "/images/dastoor/4.jpg",
     },
     {
-        img: "/assets/images/dastoor/5.jpg",
+        img: "/images/dastoor/5.jpg",
     },
     {
-        img: "/assets/images/dastoor/6.jpg",
+        img: "/images/dastoor/6.jpg",
     },
     {
-        img: "/assets/images/dastoor/7.jpg",
+        img: "/images/dastoor/7.jpg",
     },
     {
-        img: "/assets/images/dastoor/8.jpg",
+        img: "/images/dastoor/8.jpg",
     },
     {
-        img: "/assets/images/dastoor/9.jpg",
+        img: "/images/dastoor/9.jpg",
     },
     {
-        img: "/assets/images/dastoor/10.jpg",
+        img: "/images/dastoor/10.jpg",
     },
     {
-        img: "/assets/images/dastoor/11.jpg",
+        img: "/images/dastoor/11.jpg",
     },
     {
-        img: "/assets/images/dastoor/12.jpg",
+        img: "/images/dastoor/12.jpg",
     },
     {
-        img: "/assets/images/dastoor/13.jpg",
+        img: "/images/dastoor/13.jpg",
     },
     {
-        img: "/assets/images/dastoor/14.jpg",
+        img: "/images/dastoor/14.jpg",
     },
     {
-        img: "/assets/images/dastoor/15.jpg",
+        img: "/images/dastoor/15.jpg",
     },
     {
-        img: "/assets/images/dastoor/16.jpg",
+        img: "/images/dastoor/16.jpg",
     },
     {
-        img: "/assets/images/dastoor/17.jpg",
+        img: "/images/dastoor/17.jpg",
     },
     {
-        img: "/assets/images/dastoor/18.jpg",
+        img: "/images/dastoor/18.jpg",
     },
     {
-        img: "/assets/images/dastoor/19.jpg",
+        img: "/images/dastoor/19.jpg",
     },
     {
-        img: "/assets/images/dastoor/20.jpg",
+        img: "/images/dastoor/20.jpg",
     },
     {
-        img: "/assets/images/dastoor/21.jpg",
+        img: "/images/dastoor/21.jpg",
     },
     {
-        img: "/assets/images/dastoor/22.jpg",
+        img: "/images/dastoor/22.jpg",
     },
     {
-        img: "/assets/images/dastoor/23.jpg",
+        img: "/images/dastoor/23.jpg",
     },
     {
-        img: "/assets/images/dastoor/24.jpg",
+        img: "/images/dastoor/24.jpg",
     },
     {
-        img: "/assets/images/dastoor/25.jpg",
+        img: "/images/dastoor/25.jpg",
     },
     {
-        img: "/assets/images/dastoor/26.jpg",
+        img: "/images/dastoor/26.jpg",
     },
     {
-        img: "/assets/images/dastoor/27.jpg",
+        img: "/images/dastoor/27.jpg",
     },
     {
-        img: "/assets/images/dastoor/28.jpg",
+        img: "/images/dastoor/28.jpg",
     },
     {
-        img: "/assets/images/dastoor/29.jpg",
+        img: "/images/dastoor/29.jpg",
     },
 ]
