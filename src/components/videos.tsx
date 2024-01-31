4a3dbf21033be0bd54e12e1647a74a3ecb86942b
mufaqar <mufaqar@gmail.com>
2024-01-31 12:46:56 +0500

    Video

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { BsPlayCircle } from 'react-icons/bs';
 import ModelBox from './modelbox';
+import Image from 'next/image';
 
 export const VideosGallery = ({ videosData, type }: any) => {
+    console.log("🚀 ~ VideosGallery ~ videosData:", videosData)
     const [modalIsOpen, setIsOpen] = useState(false);
 
     const [URL, setURL] = useState("");
@@ -18,14 +20,14 @@ export const VideosGallery = ({ videosData, type }: any) => {
         <>
             <section className="md:flex gap-6 max-h-[600px]">
                 <div className="rounded-xl relative md:w-[77%] w-full overflow-hidden inline-block border-[10px] border-white dark:border-transparent shadow-xl">
-                    <img
-                        src={`${videosData[1]?.videoUrl?.replace(
+                    <Image
+                        src={`${videosData[0]?.videoInfo?.videoUrl?.replace(
                             "https://www.youtube.com/watch?v=",
                             "https://i.ytimg.com/vi/"
                         )}/hqdefault.jpg`}
                         alt="thumbnil"
-                        // width={900}
-                        // height={900}
+                        width={900}
+                        height={900}
                         className="w-full h-full object-cover"
                     />
                     <BsPlayCircle
@@ -36,20 +38,20 @@ export const VideosGallery = ({ videosData, type }: any) => {
                     />
                 </div>
                 <div className="flex md:flex-col mt-5 md:mt-0 justify-between gap-5 md:w-[23%] w-full">
-                    {videosData?.slice(1, 4).map(({item, idx} : any) => {
+                    {videosData?.slice(0, 3).map((item: any, idx: number) => {
                         return (
                             <div
                                 key={idx}
                                 className="rounded-xl relative md:rounded-xl h-[33%] border-[6px] border-white dark:border-transparent shadow-xl overflow-hidden inline-block"
                             >
-                                <img
-                                    src={`${item?.videoUrl?.replace(
+                                <Image
+                                    src={`${item?.videoInfo?.videoUrl?.replace(
                                         "https://www.youtube.com/watch?v=",
                                         "https://i.ytimg.com/vi/"
                                     )}/hqdefault.jpg`}
                                     alt="thumbnil"
-                                    // width={900}
-                                    // height={900}
+                                    width={360}
+                                    height={360}
                                     className="w-full h-full object-cover"
                                 />
                                 <BsPlayCircle
